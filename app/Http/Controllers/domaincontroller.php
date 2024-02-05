2d9fad812b2096311beb7257e11425342ec1bf29
Bryant Mejia <bryantmejia21@gmail.com>
2024-02-05 19:27:18 -0400

    Creando validacion

--- app/Http/Controllers/domaincontroller.php
+++ app/Http/Controllers/domaincontroller.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class domaincontroller extends Controller
 {
-    //
+    public function generateDomain(Request $req){
+       $req->validate(
+            [
+                "description"=>"required|min:3|filled"
+            ]
+        ); 
+    }   
 }
