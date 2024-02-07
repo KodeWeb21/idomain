e956eb88b96be9ae1588433117508732369953d6
Bryant Mejia <bryantmejia21@gmail.com>
2024-02-06 23:09:54 -0400

    Agregando API de openai

--- app/Http/Controllers/domaincontroller.php
+++ app/Http/Controllers/domaincontroller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use OpenAI;
 
 class domaincontroller extends Controller
 {
@@ -13,12 +14,23 @@ class domaincontroller extends Controller
             ]
         ); 
         $description = $req->description;
-        $messageTest = json_encode(
-            [
-                "domains" => ["MyDomain.es","Lolos.com","other.do"],
-                "message_send" => $description
-            ]
-            );
-        echo $messageTest;
+        $messageSystem = [
+            1 => getenv("MESSAGE_SYSTEM"),
+            2 => getenv("MESSAGE_SYSTEM2")
+        ];
+        $apiKey = getenv("API_KEY");
+        $client = OpenAI::client($apiKey);
+
+        $result = $client->chat()->create([
+            'model' => 'gpt-3.5-turbo-1106',
+            "response_format" => ["type"=>"json_object"],
+            'messages' => [
+                ['role' => 'system', 'content' => $messageSystem[1]],
+                ['role'=> 'system', 'content' => $messageSystem[2]],
+                ['role'=> 'user', 'content' => $description]
+            ],
+        ]);
+        
+        return $result->choices[0]->message->content;
     }   
 }
