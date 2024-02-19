1c77a931af0af7d055d7973ed30381e588c86ddb
Bryant Mejia <bryantmejia21@gmail.com>
2024-02-18 21:47:45 -0400

    Agregando enlace a la pagina de inicio

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,4 +1,6 @@
 <header class="header">
-        <img src="{{asset('assets/logo.svg')}}" alt="">
-        <span class="header__text">IDomain</span>
+        <a href="/" class="header__link">
+                <img src="{{asset('assets/logo.svg')}}" alt="">
+                <span class="header__text">IDomain</span>
+        </a>
 </header>
