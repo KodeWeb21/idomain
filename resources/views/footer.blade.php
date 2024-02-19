fdee7bed0e317f45c1dc1cd0103fad3f58ce0437
Bryant Mejia <bryantmejia21@gmail.com>
2024-02-18 21:17:53 -0400

    creando footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="footer">
+    <nav class="nav">
+        <ul class="nav__list">
+            <li class="nav__item">
+                <a href="">Frequently Asked Questions</a>
+            </li>
+            <li class="nav__item">
+                <a href="/privacy">Privacy</a>
+            </li>
+            <li class="nav__item">
+                <a href="">Terms and Conditions</a>
+            </li>
+            <li class="nav__item">
+                <a href="">Tips for creating domains</a>
+            </li>
+        </ul>
+    </nav>
+</footer>
